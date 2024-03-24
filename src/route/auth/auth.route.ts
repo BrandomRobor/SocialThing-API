ae9d741ad3b4933904c035d92a8176e2791d2eaa
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-03-24 16:11:05 +0000

    Catch duplicate username error

--- src/route/auth/auth.route.ts
+++ src/route/auth/auth.route.ts
@@ -1,6 +1,7 @@
 import Elysia from "elysia";
 import authService from "../../service/auth/auth.service";
 import { authRegisterRequestBody } from "./auth.schema";
+import postgres from "postgres";
 
 export default new Elysia({ name: "auth.route" })
 	.use(authService)
@@ -8,7 +9,16 @@ export default new Elysia({ name: "auth.route" })
 		app.post(
 			"/register",
 			async ({ authService, set, body }) => {
-				await authService.createUser(body);
+				try {
+					await authService.createUser(body);
+				} catch (e) {
+					if (e instanceof postgres.PostgresError && e.code === "23505") {
+						set.status = 409;
+						return { message: "Username is already taken" };
+					}
+					throw e;
+				}
+
 				set.status = 201;
 				return { message: "User was created successfully" };
 			},
