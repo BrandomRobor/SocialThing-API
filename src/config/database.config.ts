ea664f6521f3a18e43cbf82d14eabfd0d0caf7b7
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-03-24 00:32:14 +0000

    Start PostgreSQL database after API container

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ import Elysia from "elysia";
 import postgres from "postgres";
 
 const connection = postgres(
-	"postgres://postgres:postgres@0.0.0.0:5432/social-thing-api",
+	"postgres://postgres:postgres@127.0.0.1:5432/postgres",
 );
 const database = drizzle(connection);
 export default new Elysia({ name: "database.config" }).decorate({ database });
