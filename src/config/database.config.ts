a0d93e9ee077fb81bc3f680f64fb4c86398058b6
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-03-24 17:58:13 +0000

    Add Drizzle schemas to database object

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import Elysia from "elysia";
 import postgres from "postgres";
+import * as userModels from "../model/auth/auth.model";
 
 const connection = postgres(
 	"postgres://postgres:postgres@127.0.0.1:5432/postgres",
 );
-const database = drizzle(connection);
+const database = drizzle(connection, { schema: { ...userModels } });
 export default new Elysia({ name: "database.config" }).decorate({ database });
