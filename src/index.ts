7ce07db6a9845329e9975dc6e3b07f0b41fade6d
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-03-24 18:27:43 +0000

    Add Elysia Swagger plugin

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,20 @@ import authRoute from "./route/auth/auth.route";
 const port = process.env.PORT || 3000
 const app = new Elysia();
 
+if (process.env.NODE_ENV !== "production") {
+	const { swagger } = await import("@elysiajs/swagger");
+	app.use(
+		swagger({
+			documentation: {
+				info: {
+					title: "SocialThing API Docs",
+					version: "0.1.0",
+				},
+			},
+		}),
+	);
+}
+
 app.use(authRoute);
 
 app.listen(port);
