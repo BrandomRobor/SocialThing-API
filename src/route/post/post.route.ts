3fc1222eb9b948833586a8a53b987b41c0f7503b
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-04-22 01:13:21 +0000

    Create get post endpoint

--- src/route/post/post.route.ts
+++ src/route/post/post.route.ts
@@ -6,24 +6,40 @@ import * as postSchema from "./post.schema";
 export default new Elysia({ name: "post.route" })
 	.use(postService)
 	.group("/post", (app) =>
-		app.guard((app) =>
-			app
-				.use(jwtPlugin)
-				.resolve(({ userId, error }) => {
-					if (!userId || Number.isNaN(userId)) {
-						return error(401);
+		app
+			.guard((app) =>
+				app
+					.use(jwtPlugin)
+					.resolve(({ userId, error }) => {
+						if (!userId || Number.isNaN(userId)) {
+							return error(401);
+						}
+						return { userId };
+					})
+					.post(
+						"/",
+						async ({ userId, body, postService }) => {
+							const postId = await postService.createPost(body, userId);
+							return { postId };
+						},
+						{
+							body: postSchema.postPostRequestBody,
+						},
+					),
+			)
+			.get(
+				"/:id",
+				async ({ params, postService, error }) => {
+					const post = await postService.getPostById(params);
+
+					if (!post) {
+						return error(404);
 					}
-					return { userId };
-				})
-				.post(
-					"/",
-					async ({ userId, body, postService }) => {
-						const postId = await postService.createPost(body, userId);
-						return { postId };
-					},
-					{
-						body: postSchema.postPostRequestBody,
-					},
-				),
-		),
+
+					return post;
+				},
+				{
+					params: postSchema.getPostParams,
+				},
+			),
 	);
