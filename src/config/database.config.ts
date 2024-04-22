d5c6ddc414ffe24f00e0736c398bf406e95c1801
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-04-22 00:37:45 +0000

    Add post models to global drizzle instance

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,9 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import Elysia from "elysia";
 import postgres from "postgres";
 import * as userModels from "../model/auth/auth.model";
+import * as postModels from "../model/post/post.model";
 
 const connection = postgres(
 	"postgres://postgres:postgres@127.0.0.1:5432/postgres",
 );
-const database = drizzle(connection, { schema: { ...userModels } });
+const database = drizzle(connection, {
+	schema: { ...userModels, ...postModels },
+});
 export default new Elysia({ name: "database.config" }).decorate({ database });
