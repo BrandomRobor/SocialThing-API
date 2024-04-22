34f7472fec2e8a040d1744b9a39ca5d8bf7b6f81
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2024-04-22 01:38:45 +0000

    Update authentication service to work with new model changes

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -4,6 +4,7 @@ import { userPublicModel } from "../model/user-public.model";
 import type { authRegisterRequestBody } from "../schema/auth.schema";
 import { eq } from "drizzle-orm";
 import jwtPlugin from "../plugin/jwt.plugin";
+import { userSensitiveModel } from "../model/user-sensitive.model";
 
 export default new Elysia({ name: "auth.service" })
 	.use(databaseConfig)
@@ -16,24 +17,29 @@ export default new Elysia({ name: "auth.service" })
 					password,
 				}: typeof authRegisterRequestBody.static) => {
 					const encryptedPassword = await Bun.password.hash(password);
-					await database
+					const [{ userPublicId }] = await database
 						.insert(userPublicModel)
-						.values({ username, password: encryptedPassword });
+						.values({ username })
+						.returning({ userPublicId: userPublicModel.id });
+					await database
+						.insert(userSensitiveModel)
+						.values({ password: encryptedPassword, userPublicId });
 				},
 				loginUser: async ({
 					username,
 					password,
 				}: typeof authRegisterRequestBody.static) => {
-					const userData = await database.query.userModel.findFirst({
+					const userData = await database.query.userPublicModel.findFirst({
+						with: { sensitiveInformation: true },
 						where: eq(userPublicModel.username, username),
 					});
-					if (!userData) {
+					if (!userData || !userData.sensitiveInformation) {
 						return null;
 					}
 
 					const isPasswordCorrect = await Bun.password.verify(
 						password,
-						userData.password,
+						userData.sensitiveInformation.password,
 					);
 					if (!isPasswordCorrect) {
 						return null;
